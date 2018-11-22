324a6dfabaa60feae650241e52f5afbfeb5eab25
Can Celik <can.celik@outlook.com>
2018-11-22 13:53:30 +0100

    Added exact same code as a .net 4.7.1 prohect

---
+++ SampleNetFramework/Program.cs
@@ -0,0 +1,128 @@
+﻿using IdentityModel.Client;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Linq;
+
+//********************************************************************************************************************************
+//**********    ICD API allows programmatic access to the International Classification of Diseases(ICD). 
+//**********    More Information on ICD API and getting access to it 
+//**********
+//**********    https://icd.who.int/icdapi
+//**********
+//**********
+//********************************************************************************************************************************
+namespace Sample1
+{
+    class Program
+    {
+        //The _secureFile is a text file with two lines in it. The first line contains the client id and the second line client key
+        static string _secureFile = @"c:\users\can\securefile.txt";
+
+        static void Main(string[] args)
+        {
+            Sample1().GetAwaiter().GetResult();
+        }
+
+        static async Task Sample1()
+        {
+            var lines = File.ReadLines(_secureFile).ToArray();
+            if (lines.Count() != 2)
+            {
+                Console.WriteLine("the securefile should have two lines in it. The first line contains the client id and the second line client key");
+                return;
+            }
+
+            var clientId = lines[0];
+            var clientSecret = lines[1];
+
+            var client = new HttpClient();
+            var disco = await client.GetDiscoveryDocumentAsync("https://icdaccessmanagement.who.int");
+            if (disco.IsError) throw new Exception(disco.Error);
+
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(new ClientCredentialsTokenRequest
+            {
+                Address = disco.TokenEndpoint,
+                ClientId = clientId,
+                ClientSecret = clientSecret,
+                Scope = "icdapi_access",
+                GrantType = "client_credentials",
+                ClientCredentialStyle = ClientCredentialStyle.AuthorizationHeader
+            });
+
+            if (tokenResponse.IsError)
+            {
+                Console.WriteLine(tokenResponse.Error);
+                return;
+            }
+
+            Console.WriteLine(tokenResponse.Json);
+            Console.WriteLine("\n\n");
+
+            // call api
+            client = new HttpClient();
+            client.SetBearerToken(tokenResponse.AccessToken);
+
+            HttpRequestMessage request;
+
+
+            Console.WriteLine();
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine("Requesting the root foundation URI...");
+            request = new HttpRequestMessage(HttpMethod.Get, "https://id.who.int/icd/entity");
+
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Headers.AcceptLanguage.Add(new StringWithQualityHeaderValue("en"));
+            var response = await client.SendAsync(request);
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+
+            var resultJson = response.Content.ReadAsStringAsync().Result;
+            var prettyJson = JValue.Parse(resultJson).ToString(Formatting.Indented); //convert json to a more human readable fashion
+            Console.WriteLine(prettyJson);
+
+            Console.ReadKey();//Wait until a key is pressed
+
+            Console.WriteLine("****************************************************************");
+            Console.WriteLine("Enter a search term:");
+            var term = Console.ReadLine();
+            request = new HttpRequestMessage(HttpMethod.Get, "https://id.who.int/icd/release/11/beta/mms/search?q=" + term);
+
+            request.Headers.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+            request.Headers.AcceptLanguage.Add(new StringWithQualityHeaderValue("en"));
+            response = await client.SendAsync(request);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                Console.WriteLine(response.StatusCode);
+            }
+
+            resultJson = response.Content.ReadAsStringAsync().Result; //Now resultJson has the resulting json string
+            Console.WriteLine("****** Search result json *****");
+            Console.WriteLine(resultJson);
+
+            prettyJson = JValue.Parse(resultJson).ToString(Formatting.Indented); //convert json to a more human readable fashion
+            Console.WriteLine("****** And the pretty json output *****");
+            Console.WriteLine(prettyJson);
+
+            //Now trying to parse and get titles from the search result
+
+            Console.WriteLine("****** ICD code and titles from the search *****");
+            dynamic searchResult = JsonConvert.DeserializeObject(resultJson);
+
+            foreach (var de in searchResult.DestinationEntities)
+            {
+                Console.WriteLine(de.TheCode + " " + de.Title);
+            }
+
+            Console.ReadKey(); //Wait until a key is pressed
+
+        }
+    }
+}
---
+++ SampleNetFramework/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("SampleNetFramework")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SampleNetFramework")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("90b5ac75-556d-4d7e-a4c9-d33df5f1d3c2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
